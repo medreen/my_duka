60b3094f8ddb12e66fdf1aaa4270fe359f0c9046
medreen.wc <medreenwchege@gmail.com>
2025-12-02 08:40:11 +0300

    added on database

--- database.py
+++ database.py
@@ -15,21 +15,27 @@ def insert_products(values):
     cur.execute(f"insert into products(name,buying_price,selling_price) values{values}")
     conn.commit()
 
-my_product = ('Samsung', 100000, 120000)
-my_product2 = ('Iphone', 200000, 250000)
-insert_products(my_product)
-insert_products(my_product2)
+# my_product = ('Samsung', 100000, 120000)
+# my_product2 = ('Iphone', 200000, 250000)
+# insert_products(my_product)
+# insert_products(my_product2)
 print(products)
 
 def insert_sales(values):
     cur.execute(f"insert into sales(pid, quantity)values{values}")
     conn.commit()
 
-my_sale = (1, 700)
-my_sale2 = (2, 49)
-insert_sales(my_sale)
-insert_sales(my_sale2)
+# my_sale = (1, 700)
+# my_sale2 = (2, 49)
+# insert_sales(my_sale)
+# insert_sales(my_sale2)
 
+def insert_sales_2(values):
+    cur.execute("insert into sales(pid,quantity)values(%s,%s)",(values))
+    conn.commit()
+
+# sale_1 = (49,200)
+# insert_sales_2(sale_1)
 
 def get_sales():
     cur.execute("select * from sales")
