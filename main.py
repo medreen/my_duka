366f5ef7047cc412b1dfd394160acb95f447dbe4
medreen.wc <medreenwchege@gmail.com>
2025-12-09 16:53:17 +0300

    made changes on mainpy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-from database import get_products,get_sales
+from flask import Flask, render_template, request, redirect, url_for
+from database import get_products,get_sales, insert_products
 
-# assigning an object to flask
+# assigning an object to flask/ instance of flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,15 @@ def home():
 def fetch_products():
     products = get_products()
     return render_template("products.html", products = products)
+
+@app.route('/add_products', methods = ['GET', 'POST'])
+def add_products():
+    product_name = request.form['product_name']
+    buying_price = request.form["buying_price"]
+    selling_price = request.form["selling_price"]
+    new_product = (product_name, buying_price, selling_price)
+    insert_products(new_product)
+    return redirect(url_for('fetch_products'))
 
 @app.route('/sales')
 def fetch_sales():
